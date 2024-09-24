c90d3837842f7dd0070b8152f8bd8ede1915f031
Eetu Miettinen <eetu.miettinen@savonia.fi>
2024-09-24 16:35:06 +0300

    Update shut_up.py
    full path for systemctl

--- misc/shut_up.py
+++ misc/shut_up.py
@@ -1,9 +1,11 @@
 # stop the beeping at startup
 import sys
-import os
+#import os
 
 # Get the absolute path of the directory containing the module
-module_path = os.path.abspath(os.path.join('..', 'main', 'control_modules'))
+#module_path = os.path.abspath(os.path.join('..', 'main', 'control_modules'))
+
+module_path = 'home/pi/Documents/masi/main/control_modules'
 
 # Add it to the Python path
 if module_path not in sys.path:
@@ -16,7 +18,6 @@ path = '../main/configuration_files/excavator_channel_configs.yaml'
 
 pwm = PWM_hat(
     config_file=path,
-    inputs=8,
     simulation_mode=False,
 	input_rate_threshold=0,	# set rate to 0 (or None) to disable rate monitoring
 )
